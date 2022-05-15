0f434102c80e0c2c1693a22bc1fd63b189520019
Kenji Saito <ken-yo@mbr.nifty.com>
2022-05-15 22:09:26 +0900

    Add the analyzer

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
 const withMDX = require('@next/mdx')({
     extension: /\.mdx?$/,
-})
-module.exports = withMDX({
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
-    webpack5: true,
-    reactStrictMode: true,
-    esmExternals: true,
-    swcLoader: true,
-    swcMinify: true,
-})
+});
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(withMDX({
+        pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx', 'md'],
+        webpack5: true,
+        reactStrictMode: true,
+        esmExternals: true,
+        swcLoader: true,
+        swcMinify: true,
+        experimental: {
+            modern: true,
+        }
+    }
+));
