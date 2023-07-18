c8e9e58fb4467ac01e5b52620e5c4f4055d73bb2
Kenji Saito <ken-yo@mbr.nifty.com>
2023-07-19 04:21:34 +0900

    Bumps node modules

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,11 +1,13 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth/next/index";
 import GithubProvider from 'next-auth/providers/github';
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string,
     }),
   ],
-});
+};
+
+export const { handlers: { GET, POST }, auth } = NextAuth(authOptions);
