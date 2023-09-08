772d04e03e65e91ec3fceeeae73c2bf10073cc97
Kenji Saito <ken-yo@mbr.nifty.com>
2023-09-09 08:47:21 +0900

    Fix the workflow error

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,13 +1,16 @@
 import NextAuth from "next-auth/next/index";
-import GithubProvider from 'next-auth/providers/github';
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string,
-    }),
-  ],
+	providers: [
+		GithubProvider({
+			clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
+			clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string,
+		}),
+	],
 };
 
-export const { handlers: { GET, POST }, auth } = NextAuth(authOptions);
+export const {
+	handlers: { GET, POST },
+	auth,
+} = NextAuth(authOptions);
--- src/components/WithAuth.tsx
+++ src/components/WithAuth.tsx
@@ -1,46 +1,46 @@
-import { PropsWithChildren } from 'react';
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { Box, Button } from '@mui/material';
+import { PropsWithChildren } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Box, Button } from "@mui/material";
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 type WithAuthProps = {};
 
 export default function WithAuth(
-  props: PropsWithChildren<WithAuthProps>,
+	props: PropsWithChildren<WithAuthProps>,
 ): JSX.Element {
-  const { data: session } = useSession();
+	const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        <Box>
-          Signed in as {session?.user?.email} <br />
-          <Button
-            sx={{
-              backgroundColor: '#009900',
-            }}
-            onClick={() => signOut()}
-          >
-            Sign out
-          </Button>
-        </Box>
+	if (session) {
+		return (
+			<>
+				<Box>
+					Signed in as {session?.user?.email} <br />
+					<Button
+						sx={{
+							backgroundColor: "#009900",
+						}}
+						onClick={() => signOut()}
+					>
+						Sign out
+					</Button>
+				</Box>
 
-        <>{props.children}</>
-      </>
-    );
-  } else {
-    return (
-      <Box>
-        Not signed in <br />
-        <Button
-          sx={{
-            backgroundColor: '#009900',
-          }}
-          onClick={() => signIn('github')}
-        >
-          Sign in
-        </Button>
-      </Box>
-    );
-  }
+				{props.children}
+			</>
+		);
+	} else {
+		return (
+			<Box>
+				Not signed in <br />
+				<Button
+					sx={{
+						backgroundColor: "#009900",
+					}}
+					onClick={() => signIn("github")}
+				>
+					Sign in
+				</Button>
+			</Box>
+		);
+	}
 }
