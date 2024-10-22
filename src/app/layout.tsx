eba7720521e7503da7a6caa40d91d5950c9f0e4d
Kenji Saito <ken-yo@mbr.nifty.com>
2024-10-22 23:45:24 +0900

    Bumps node modules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,53 @@
-import type { ReactNode } from "react";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { SessionProvider } from "next-auth/react";
-import theme from "./theme";
-import StyledJsxRegistry from "./registry";
+import type { ReactNode } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SessionProvider } from 'next-auth/react';
+import theme from './theme';
+import StyledJsxRegistry from './registry';
 
 export default function Layout({
-	children,
+  children,
 }: { children: ReactNode }): JSX.Element {
-	return (
-		<html lang="en">
-			<head>
-				{/* PWA primary color */}
-				<meta name="theme-color" content={theme.palette.primary.main} />
-				{/* eslint-disable-next-line @next/next/no-page-custom-font */}
-				<link
-					rel="stylesheet"
-					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-				/>
-				{/* eslint-disable-next-line @next/next/no-page-custom-font */}
-				<link rel="preconnect" href="https://fonts.googleapis.com" />
-				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link
-					href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100&display=swap"
-					rel="stylesheet"
-				/>
-				{/* eslint-disable-next-line @next/next/no-page-custom-font */}
-				<link
-					rel="stylesheet"
-					type="text/css"
-					href="http://mplus-fonts.sourceforge.jp/webfonts/general-j/mplus_webfonts.css"
-				/>
-			</head>
-			<body
-				style={{
-					fontKerning: "normal",
-					MozFontFeatureSettings: "kern",
-					fontFeatureSettings: "kern",
-					fontFamily: "IBM Plex Sans, sans-serifM+ Type-1 (general-j) Regular",
-				}}
-			>
-				<StyledJsxRegistry>
-					<SessionProvider session={undefined}>
-						<ThemeProvider theme={theme}>
-							<CssBaseline />
-							{children}
-						</ThemeProvider>
-					</SessionProvider>
-				</StyledJsxRegistry>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={theme.palette.primary.main} />
+        { }
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        { }
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@100&display=swap"
+          rel="stylesheet"
+        />
+        { }
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="http://mplus-fonts.sourceforge.jp/webfonts/general-j/mplus_webfonts.css"
+        />
+      </head>
+      <body
+        style={{
+          fontKerning: 'normal',
+          MozFontFeatureSettings: 'kern',
+          fontFeatureSettings: 'kern',
+          fontFamily: 'IBM Plex Sans, sans-serifM+ Type-1 (general-j) Regular',
+        }}
+      >
+        <StyledJsxRegistry>
+          <SessionProvider session={undefined}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              {children}
+            </ThemeProvider>
+          </SessionProvider>
+        </StyledJsxRegistry>
+      </body>
+    </html>
+  );
 }
--- src/components/WithAuth.tsx
+++ src/components/WithAuth.tsx
@@ -1,45 +1,45 @@
-import type { PropsWithChildren } from "react";
-import { useSession, signIn, signOut } from "next-auth/react";
-import { Box, Button } from "@mui/material";
+import type { PropsWithChildren } from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { Box, Button } from '@mui/material';
 
-// biome-ignore lint/complexity/noBannedTypes: reason
-type WithAuthProps = {};
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface WithAuthProps {};
 
 export default function WithAuth(
-	props: PropsWithChildren<WithAuthProps>,
+  props: PropsWithChildren<WithAuthProps>,
 ): JSX.Element {
-	const { data: session } = useSession();
+  const { data: session } = useSession();
 
-	if (session) {
-		return (
-			<>
-				<Box>
-					Signed in as {session?.user?.email} <br />
-					<Button
-						sx={{
-							backgroundColor: "#009900",
-						}}
-						onClick={() => signOut()}
-					>
-						Sign out
-					</Button>
-				</Box>
+  if (session) {
+    return (
+      <>
+        <Box>
+          Signed in as {session?.user?.email} <br />
+          <Button
+            sx={{
+              backgroundColor: '#009900',
+            }}
+            onClick={() => signOut()}
+          >
+            Sign out
+          </Button>
+        </Box>
 
-				{props.children}
-			</>
-		);
-	}
-	return (
-		<Box>
-			Not signed in <br />
-			<Button
-				sx={{
-					backgroundColor: "#009900",
-				}}
-				onClick={() => signIn("github")}
-			>
-				Sign in
-			</Button>
-		</Box>
-	);
+        {props.children}
+      </>
+    );
+  }
+  return (
+    <Box>
+      Not signed in <br />
+      <Button
+        sx={{
+          backgroundColor: '#009900',
+        }}
+        onClick={() => signIn('github')}
+      >
+        Sign in
+      </Button>
+    </Box>
+  );
 }
