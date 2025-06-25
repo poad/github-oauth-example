0718f96b30280b33aab6e6410db908b249d00240
Kenji Saito <ken-yo@mbr.nifty.com>
2025-06-25 22:06:57 +0900

    Bumps node modules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,8 +5,6 @@ import nextPlugin from '@next/eslint-plugin-next';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
 import stylistic from '@stylistic/eslint-plugin';
-import stylisticTs from '@stylistic/eslint-plugin-ts';
-import stylisticJsx from '@stylistic/eslint-plugin-jsx';
 
 import tseslint from 'typescript-eslint';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -35,8 +33,8 @@ export default tseslint.config(
       'jsx-a11y': jsxA11yPlugin,
       '@next/next': nextPlugin,
       '@stylistic': stylistic,
-      '@stylistic/ts': stylisticTs,
-      '@stylistic/jsx': stylisticJsx,
+      '@stylistic/ts': stylistic,
+      '@stylistic/jsx': stylistic,
     },
     extends: [
       ...compat.config(reactHooksPlugin.configs.recommended),
